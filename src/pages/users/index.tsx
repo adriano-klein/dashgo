114ee7eeae3bb82fd29230b358b887b38dd95d4d
adriano25klein@gmail.com <you@example.com>
2022-05-13 09:00:47 -0300

    Lógica de paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,33 +1,14 @@
 import {Text, Checkbox, Box, Button, Flex, Heading, Icon, Table, Th, Thead, Tr, Tbody, Td, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query';
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
-
+import { useUsers } from "../../services/hooks/useUsers";
 
 
 export default function UserList(){
-  const { data, isLoading, isError, isFetching } = useQuery('users', async() => {
-     
-    const { data } =  await api.get('users');
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-        day:'2-digit',
-        month:'long',
-        year:'numeric'
-      })
-      }
-    })
-     return users;
-  })
+  const { data, isLoading, isError, isFetching } = useUsers()
   
   const isWideVersion = useBreakpointValue({
     base: false,
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 import { faker } from '@faker-js/faker';
 
 type User = {
@@ -28,7 +28,7 @@ export function makeServer(){
     },
 
     seeds(server){
-      server.createList('user', 10)
+      server.createList('user', 200)
     },
 
     routes(){
@@ -36,9 +36,22 @@ export function makeServer(){
       this.namespace = 'api'
       this.timing = 750;
 
-      this.get('/users');
-      this.post('/users');
+      this.get('/users', function(schema, request) {
+        const {page = 1, per_page = 10} = request.queryParams;
+        const total = schema.all('user').length
+        const startPage = (Number(page) -1) * Number(per_page);
+        const endPage = startPage + Number(per_page);
+        const users = this.serialize(schema.all('user')).users.slice(startPage, endPage)
+        
+        return new Response(
+        200,
+        {'x-total-count':String(total)},
+        {users},
+        )
+      });
 
+
+      this.post('/users');
       this.namespace = '';
       this.passthrough();
     }
